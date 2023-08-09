83b5c5facbf64cd50191be406e7a842523623527
Aiden <azurefalctis@proton.me>
2023-08-09 16:51:18 +0100

    Fixed bug and cleaned up code

--- Add_Task.js
+++ Add_Task.js
@@ -1,12 +1,13 @@
-var btn = document.querySelector(".add-new");
+let btn = document.querySelector(".add-new");
 btn.addEventListener("click", addTask);
 
-
 function createTaskDiv(textEntry) {
+
+    
     
     mainBody = document.querySelector(".main-body");
     
-    task = document.createElement('div');
+    const task = document.createElement('div');
     task.setAttribute("class", "task");
     taskContainer = document.createElement('div');
     taskContainer.setAttribute("class", "task-container");
@@ -14,28 +15,28 @@ function createTaskDiv(textEntry) {
     mainBody.appendChild(task);
     
 
-    textContainer = document.createElement("div");
+    const textContainer = document.createElement("div");
     textContainer.setAttribute("class", "text-container");
     taskContainer.appendChild(textContainer);
     
-    text = document.createElement("p");
+    const text = document.createElement("p");
     text.textContent = textEntry;
     text.setAttribute("class", "task-text");
     textContainer.appendChild(text);
 
-    deleteContainer = document.createElement("div");
+    const deleteContainer = document.createElement("div");
     deleteContainer.setAttribute("class", "delete-container");
-    deleteButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
     deleteButton.setAttribute("class", "delete-button");
     deleteButton.textContent = "Delete";
     deleteContainer.appendChild(deleteButton);
     taskContainer.appendChild(deleteContainer);
 
     deleteButton.addEventListener("click", (event) =>{
-        deleteButton = event.target
-        parent = deleteButton.parentElement.parentElement.parentElement
-        taskText = parent.childNodes[0].childNodes[0].childNodes[0].textContent
-        tasksInLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+        const button = event.target
+        const parent = button.parentElement.parentElement.parentElement
+        const taskText = parent.childNodes[0].childNodes[0].childNodes[0].textContent
+        const tasksInLocalStorage = JSON.parse(localStorage.getItem("tasks"));
         arrayWithoutElement = tasksInLocalStorage.filter(function (word) {
             return word !== taskText
         });
@@ -47,19 +48,17 @@ function createTaskDiv(textEntry) {
         
     });
 
-    
-
 }
 
 
 function saveItems(textEntry) {
-    var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    let tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
     tasks.push(textEntry);
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function addTask() {
-    textEntry = document.querySelector(".text-input").value;
+    let textEntry = document.querySelector(".text-input").value;
     
     createTaskDiv(textEntry);
     saveItems(textEntry);
